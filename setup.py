1d5fa89291a4828adfb27a0e9f0d6f29107b76f8
Ray Holder <ray@blacklocus.com>
2015-04-18 22:44:13 -0500

    add other supported classifiers for pypi

--- setup.py
+++ setup.py
@@ -11,6 +11,8 @@ classifiers = [
     'Programming Language :: Python',
     'Programming Language :: Python :: 2.6',
     'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
     'Topic :: Internet',
     'Topic :: Utilities',
 ]
